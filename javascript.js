5cbb8b03d95320adc78ec64441b92eed385914c4
TripleS <triple-s-76@outlook.com>
2022-08-25 05:45:50 -0700

    Finished javascript fundamentals part 1

---
+++ javascript.js
@@ -0,0 +1,161 @@
+/*
+
+console.log("hi it worked")
+
+let message = "testing the message";
+
+console.log(message);
+
+let user = "John"
+let age = 25
+message = "Hi I am John";
+
+console.log(message);
+
+message = user + " " + age;
+
+console.log(message);
+
+let admin, named;
+named = "John";
+admin = named;
+console.log(admin);
+
+let ourPlanetName = "Earth";
+let currentUserName = "John";
+
+console.log(currentUserName + " is from " + ourPlanetName)
+
+let x = 8;
+let y = 3;
+let z = x % y;
+
+console.log(z);
+
+let x = 5;
+let z = x ** 2;
+
+console.log(z);
+
+let a = Math.pow(5,3);
+
+console.log(a);
+
+
+
+let x = (100 + 50) * 3;
+
+console.log(x);
+
+let x = 123;
+let y = new Number(123);
+
+console.log(x);
+console.log(y);
+
+console.log(typeof x)
+console.log(typeof y);
+
+console.log(x==y);
+console.log(x===y);
+
+
+let x = new Number(123);
+let y = new Number(123);
+
+console.log(x==y);
+
+let myInt = 5;
+let myFloat = 6.667;
+console.log(myInt);
+console.log(myFloat);
+console.log(typeof myInt);
+console.log(typeof myFloat);
+
+let x = myFloat.toFixed(5);
+console.log(x);
+
+let x = "557";
+console.log(x);
+console.log(x + 3);
+console.log(Number(x) + 3);
+
+
+let x = 6;
+x /= 1.5;
+console.log(x);
+
+let a = 8;
+let b = 16;
+let c = 9;
+let d = 7;
+
+let e = a + b;
+console.log(e);
+
+let f = c - d;
+console.log(f);
+
+let g = e * f;
+
+console.log(g);
+
+let a = (true + true) * (true + true + true);
+let b = false + false;
+console.log(a);
+console.log(b);
+
+console.log(39 + 32);
+
+console.log(1+2+3+4+5+6);
+
+console.log((4 + 6 + 9) / 77);
+
+let a = 10;
+
+console.log(9 * a);
+
+let b = 7 * a;
+
+console.log(b);
+
+*/
+
+const max = 57;
+
+console.log(max);
+
+let actual = max - 13;
+
+console.log(actual);
+
+let percentage = actual / max;
+
+console.log(percentage);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
